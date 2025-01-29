06fd30b483dd58513234afdb4c316bed297ca02f
Kinz Sil <82054121+KXzeno@users.noreply.github.com>
2025-01-29 13:32:16 -0800

    Add route for base icons

---
+++ app/components/Icons/base/index.ts
@@ -0,0 +1,5 @@
+import { Alteration, Conjuration, Illusion, Restoration, Destruction, Enchanting } from './BaseCats';
+
+export {
+  Alteration, Conjuration, Illusion, Restoration, Destruction, Enchanting,
+}

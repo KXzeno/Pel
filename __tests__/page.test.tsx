9f8d47ceb5a613978e139a6bd4189680cac232c3
Kinz Sil <82054121+KXzeno@users.noreply.github.com>
2025-02-03 09:54:22 -0800

    test: add prospective fetch mock

--- __tests__/page.test.tsx
+++ __tests__/page.test.tsx
@@ -4,6 +4,10 @@ import userEvent from '@testing-library/user-event';
 
 import Page from "../app/page";
 
+// import 'getUserData' from ...;
+
+// jest.mock(getUserData);
+
 describe('Landing page component', () => {
   test('renders Home', async () => {
     render(<Page />);
@@ -16,3 +20,43 @@ describe('Landing page component', () => {
     expect(icons).toHaveLength(6);
   })
 });
+
+describe.skip('Profile Data Fetcher', () => {
+  test('fetches profile data from database', async () => {
+    let userData = {};
+
+    const promise = Promise.resolve({ data: { userData } });
+
+    //@ts-expect-error
+    getProfileData.mockImplementationOnce(promise);
+
+    //@ts-expect-error
+    render(<SomeComponent/>);
+
+    await userEvent.click(screen.getByRole('button'));
+
+    //@ts-expect-error
+    waitFor(() => promise);
+
+    expect(screen.getAllByRole('...')).toHaveLength(0);
+  });
+
+  test('fetches profile data from database then fail', async () => {
+    //@ts-expect-error
+    getProfileData.mockImplementationOnce(() => {
+      Promise.reject(new Error());
+    });
+
+    //@ts-expect-error
+    render(<SomeComponent/>)
+
+    await userEvent.click(screen.getByRole('button'));
+
+    //@ts-expect-error
+    let msg = await screen.findByTest(/Something went wrong/);
+
+    //@ts-expect-error
+    expect(message).toBeInTheDocument();
+  });
+});
+

76510b1c2fdb7eb0796df9e2a1e66615c93662f9
Kinz Sil <82054121+KXzeno@users.noreply.github.com>
2025-02-03 08:54:51 -0800

    Add group role for main content

--- app/page.tsx
+++ app/page.tsx
@@ -11,13 +11,13 @@ import {
 
 export default function Home() {
   return (
-    <>
+    <div role='group'>
       <Alteration size={64} />
       <Destruction size={64} />
       <Illusion size={64} />
       <Conjuration size={64} />
       <Restoration size={64} />
       <Enchanting size={64} />
-    </>
+    </div>
   );
 }

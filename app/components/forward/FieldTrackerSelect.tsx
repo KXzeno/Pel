19323a9d9607bfa7c92e079aff85f2a1e1312c3f
Kinz Sil <82054121+KXzeno@users.noreply.github.com>
2025-02-04 05:31:22 -0800

    Init note-type selection RFC

---
+++ app/components/forward/FieldTrackerSelect.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { 
+  Alteration, 
+  Destruction, 
+  Illusion, 
+  Conjuration, 
+  Restoration, 
+  Enchanting 
+} from '@/Icons/base';
+
+export default function FieldTrackerSelect() {
+  return (
+    <div role='group'>
+      <Alteration size={64} />
+      <Destruction size={64} />
+      <Illusion size={64} />
+      <Conjuration size={64} />
+      <Restoration size={64} />
+      <Enchanting size={64} />
+    </div>
+  );
+}
+

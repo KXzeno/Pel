ef9642230c44c6c2763ee0f4338c1689707e7732
Kinz Sil <82054121+KXzeno@users.noreply.github.com>
2025-02-04 05:29:56 -0800

    Extract component

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,11 @@
 import Image from "next/image";
 
-import { 
-  Alteration, 
-  Destruction, 
-  Illusion, 
-  Conjuration, 
-  Restoration, 
-  Enchanting 
-} from '@/Icons/base';
+import { Landing } from "@main";
 
 export default function Home() {
   return (
-    <div role='group'>
-      <Alteration size={64} />
-      <Destruction size={64} />
-      <Illusion size={64} />
-      <Conjuration size={64} />
-      <Restoration size={64} />
-      <Enchanting size={64} />
-    </div>
+    <main role="main">
+      <Landing />
+    </main>
   );
 }

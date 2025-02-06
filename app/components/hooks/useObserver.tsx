43230d12e0a325e86414ca55d478ebee935ceeb1
KXzeno <82054121+KXzeno@users.noreply.github.com>
2025-02-06 12:43:29 -0800

    Init `useObserver` hook
    - Create stepper function for number literal-type parsing

--- app/components/hooks/useObserver.tsx
+++ app/components/hooks/useObserver.tsx
@@ -1,5 +1,20 @@
 import React from 'react';
 
+/** @remarks
+ * Arithmetic expressions cannot be performed in type definitions,
+ * which shouldn't anyway. The quirky nature of the stepper type above 
+ * is that it uses the field 'length' to step. To automate decimal union 
+ * types, we will create a cocnrete stepper function and access it's types */
+export function createNumberedArray(start: number, end: number, step: number = 1): Array<number> {
+  let arr = [start];
+  while (arr[arr.length - 1] < end) {
+    // Ensure .1 incrementation by pre/post mapping with multiple of 10
+    let val = (arr[arr.length - 1] * 10 + step * 10) / 10;
+    arr.push(val);
+  }
+  return arr;
+};
+
 export default function useObserver<T extends HTMLElement = HTMLDivElement>(elem: T) {
 
   type ObserverRootMargin = `${number}px`;
@@ -15,15 +30,19 @@ export default function useObserver<T extends HTMLElement = HTMLDivElement>(elem
    * via `Range[number]` that enumerates the array for different types
    *
    * With love to Java's strict typing, this foreign complexity
-   * of flexible typing is a delightful reprieve
+   * of flexible typing is a delightful reprieve, however it cannot
+   * handle decimal stepping, thus requiring the external function
    * */
-  type ObserverThreshold<Max extends number, Range extends Array<unknown> = []> = Range['length'] extends Max ? Range[number] : ObserverThreshold<Max, [...Range, Range['length']]>;
+  type CreateRange<Max extends number, Range extends Array<number> = []> = Range['length'] extends Max ? Range[number] : CreateRange<Max, [...Range, Range['length']]>;
+
+  type ObserverThreshold = CreateRange<2>;
 
   type ObserverOptions = {
     root: ObserverRoot;
     rootMargin: ObserverRootMargin;
-    threshold: ObserverThreshold<2>;
+    threshold: ObserverThreshold;
   }
+
   let options: ObserverOptions = {
     root: elem,
     rootMargin: "2px",

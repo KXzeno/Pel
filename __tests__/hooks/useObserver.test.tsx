2bf8bf21f93dbedc3cf7053af93282dbc55188fe
KXzeno <82054121+KXzeno@users.noreply.github.com>
2025-02-07 11:11:44 -0800

    refactor: reinitialize step accumulator

--- __tests__/hooks/useObserver.test.tsx
+++ __tests__/hooks/useObserver.test.tsx
@@ -1,11 +1,13 @@
 import "@testing-library/jest-dom";
 
-import useObserver, { createNumberedArray } from '../../app/components/hooks/useObserver';
+import useObserver from '../../app/components/hooks/useObserver';
+import StepAccumlator from 'utils/StepAccumulator';
 
 describe("A dynamic observer intersection hook", () => {
   test('creates a numbered array', () => {
-    let numberedArrayDecimal = createNumberedArray(0, 1, 0.1);
-    let numberedArrayInteger = createNumberedArray(0, 10);
+    let acc = new StepAccumlator([1 as const]);
+    let numberedArrayDecimal = acc.createNumberedArray(0, 1, 0.1);
+    let numberedArrayInteger = acc.createNumberedArray(0, 10);
     let intendedOutputForDecimal = [0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1];
     let intendedOutputForInteger = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 

6669d1dd0bbdd4becf6c40f2c9de105648524d4f
KXzeno <82054121+KXzeno@users.noreply.github.com>
2025-02-07 20:53:14 -0800

    docs: add tentative global observer mock

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,16 @@
 import '@testing-library/jest-dom';
+
+/** Globally set observer mock
+ *
+// @ts-expect-error
+global.IntersectionObserver = class {
+  public callback;
+  constructor(callback: IntersectionObserverCallback) {
+    this.callback = callback;
+  }
+
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+} 
+*/

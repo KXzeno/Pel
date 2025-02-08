1c5e21f9101d0e07b0772623ecd6bb072622a5e2
KXzeno <82054121+KXzeno@users.noreply.github.com>
2025-02-07 20:52:03 -0800

    test(fix): mock intersection observer
    - IntersectionObserveer isn't defined in jestdom

--- __tests__/snapshot.tsx
+++ __tests__/snapshot.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import Page from '../app/page';
- 
+
+const mockIntersectionObserver = jest.fn();
+mockIntersectionObserver.mockReturnValue({
+  observe: () => null,
+  unobserve: () => null,
+  disconnect: () => null
+});
+window.IntersectionObserver = mockIntersectionObserver;
+
 it('renders homepage unchanged', () => {
   const { container } = render(<Page />)
   expect(container).toMatchSnapshot()

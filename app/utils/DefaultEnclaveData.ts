410674144759490c13e1b26af4688facf2e615b8
KXzeno <82054121+KXzeno@users.noreply.github.com>
2025-02-21 07:57:28 -0800

    chore: add required prop for type conformity

--- app/utils/DefaultEnclaveData.ts
+++ app/utils/DefaultEnclaveData.ts
@@ -4,55 +4,63 @@ export interface EnclaveData {
   [index: `${number}C${number}` | `${number}C${number}:${number}`]: object;
 }
 
-export const defaultData: EnclaveData[] = [
-  {
-    id: 'E1',
-    name: 'PowerShell',
-    '1C1': {
-      data: { value: 'sup', },
-      T1: { data:{}, },
-      '2C1:1': { data: {}, T1: { data: {}, }, T2: { data: {}, }, },
-      '2C2:1': { data:{}, T1: { data: {}, }, '3C1:2': { data: {}, }, },
-    }
-  },
-  {
-    id: 'E2',
-    name: 'Vim/Neovim',
-    '1C1': {
-      data: { value: 'sup', },
-      T1: { data:{}, },
-      '2C1:1': { data: {}, T1: { data: {}, }, T2: { data: {}, }, },
-      '2C2:1': { data:{}, T1: { data: {}, }, '3C1:2': { data: {}, }, },
-    }
-  },
-  {
-    id: 'E3',
-    name: 'Starcraft II',
-    '1C1': {
-      data: { value: 'sup', },
-      T1: { data:{}, },
-      '2C1:1': { data: {}, T1: { data: {}, }, T2: { data: {}, }, },
-      '2C2:1': { data:{}, T1: { data: {}, }, '3C1:2': { data: {}, }, },
-    }
-  },
-  {
-    id: 'E4',
-    name: 'Field Notes',
-    '1C1': {
-      data: { value: 'sup', },
-      T1: { data:{}, },
-      '2C1:1': { data: {}, T1: { data: {}, }, T2: { data: {}, }, },
-      '2C2:1': { data:{}, T1: { data: {}, }, '3C1:2': { data: {}, }, },
-    }
-  },
-  {
-    id: 'E5',
-    name: 'Project - Karnovah',
-    '1C1': {
-      data: { value: 'sup', },
-      T1: { data:{}, },
-      '2C1:1': { data: {}, T1: { data: {}, }, T2: { data: {}, }, },
-      '2C2:1': { data:{}, T1: { data: {}, }, '3C1:2': { data: {}, }, },
-    }
-  },
-];
+interface DefaultData {
+  loadedModules: EnclaveData[];
+  moduleAdderInput: string;
+}
+
+export const defaultData: DefaultData = { 
+  loadedModules: [
+    {
+      id: 'E1',
+      name: 'PowerShell',
+      '1C1': {
+        data: { value: 'sup', },
+        T1: { data:{}, },
+        '2C1:1': { data: {}, T1: { data: {}, }, T2: { data: {}, }, },
+        '2C2:1': { data:{}, T1: { data: {}, }, '3C1:2': { data: {}, }, },
+      }
+    },
+    {
+      id: 'E2',
+      name: 'Vim/Neovim',
+      '1C1': {
+        data: { value: 'sup', },
+        T1: { data:{}, },
+        '2C1:1': { data: {}, T1: { data: {}, }, T2: { data: {}, }, },
+        '2C2:1': { data:{}, T1: { data: {}, }, '3C1:2': { data: {}, }, },
+      }
+    },
+    {
+      id: 'E3',
+      name: 'Starcraft II',
+      '1C1': {
+        data: { value: 'sup', },
+        T1: { data:{}, },
+        '2C1:1': { data: {}, T1: { data: {}, }, T2: { data: {}, }, },
+        '2C2:1': { data:{}, T1: { data: {}, }, '3C1:2': { data: {}, }, },
+      }
+    },
+    {
+      id: 'E4',
+      name: 'Field Notes',
+      '1C1': {
+        data: { value: 'sup', },
+        T1: { data:{}, },
+        '2C1:1': { data: {}, T1: { data: {}, }, T2: { data: {}, }, },
+        '2C2:1': { data:{}, T1: { data: {}, }, '3C1:2': { data: {}, }, },
+      }
+    },
+    {
+      id: 'E5',
+      name: 'Project - Karnovah',
+      '1C1': {
+        data: { value: 'sup', },
+        T1: { data:{}, },
+        '2C1:1': { data: {}, T1: { data: {}, }, T2: { data: {}, }, },
+        '2C2:1': { data:{}, T1: { data: {}, }, '3C1:2': { data: {}, }, },
+      }
+    },
+  ],
+  moduleAdderInput: '',
+};

255063cb874d2091e6c99f5623acc672cbb8aad0
KXzeno <82054121+KXzeno@users.noreply.github.com>
2025-02-22 10:24:27 -0800

    Camelcase `Icons` and move to `forward`

--- app/components/Icons/base/index.ts
+++
@@ -1,5 +0,0 @@
-import { Alteration, Conjuration, Illusion, Restoration, Destruction, Enchanting } from './BaseCats';
-
-export {
-  Alteration, Conjuration, Illusion, Restoration, Destruction, Enchanting,
-}

422eccc6976331f51e60966fed55f1ba5544ea1a
KXzeno <82054121+KXzeno@users.noreply.github.com>
2025-02-22 13:13:44 -0800

    feat: add action type and type predicates

--- app/utils/auxil/enclaveReducer.tsx
+++ app/utils/auxil/enclaveReducer.tsx
@@ -8,10 +8,10 @@ import {
 export default function enclaveReducer(state: ReducerState, action: ReducerAction): ReducerState {
   switch (action.type) {
     case DispatcherMode.ModuleInput: {
-      if (action.payload && action.payload.data) {
+      if (action.payload && isString(action.payload)) {
         return {
           ...state,
-          moduleAdderInput: action.payload.data as string,
+          moduleAdderInput: action.payload.data,
         }
       }
       return { ...state, moduleAdderInput: '' };
@@ -22,13 +22,12 @@ export default function enclaveReducer(state: ReducerState, action: ReducerActio
       return { ...state };
     }
     case DispatcherMode.ModuleInputSubmit: {
-      console.log(action.payload);
-      if (!action.payload) {
+      if (!isString(action.payload)) {
         throw new Error('No input parsed.');
       }
 
       // TODO: Properly submissions
-      if ((action.payload.data as string).length === 0) {
+      if ((action.payload.data).length === 0) {
         return { ...state };
       }
 
@@ -43,7 +42,46 @@ export default function enclaveReducer(state: ReducerState, action: ReducerActio
       // Add deletion marker array to reclaim enclave ids,
       // perhaps with bonus syntax that indicates reclaim
 
-      return { loadedModules: [...state.loadedModules, newModule], moduleAdderInput: '' };
+      return { loadedModules: [...state.loadedModules, newModule], moduleAdderInput: '', isInputVisible: false };
+    }
+    case DispatcherMode.ShowModuleInput: {
+      if (!isButtonElement(action.payload)) {
+        throw new Error('Button event misfired.')
+      }
+      const { data: elem } = action.payload;
+      const btnType = elem.getAttribute('type');
+
+      if (btnType === 'button' && state.isInputVisible === false) {
+        elem.setAttribute('type', 'submit');
+        return { ...state, isInputVisible: true };
+      } else if (btnType === 'submit' && state.moduleAdderInput.length > 0) {
+        elem.click();
+        elem.setAttribute('type', 'button');
+        return { ...state, isInputVisible: false };
+      }
+      return { ...state, isInputVisible: true };
     }
   }
+}
+
+/**
+ *
+ */
+function isButtonElement(payload: ReducerAction['payload']): payload is { data: HTMLButtonElement } {
+  return payload ? payload.data instanceof HTMLElement : false;
+}
+
+
+/**
+ *
+ */
+function isString(payload: ReducerAction['payload']): payload is { data: string } {
+  return payload ? typeof payload.data === 'string' : false;
+}
+
+/**
+ *
+ */
+function isEnclaveData(payload: ReducerAction['payload']): payload is { data: EnclaveData } {
+  return payload ? Object.keys(payload.data)[0] === 'id' : false;
 }

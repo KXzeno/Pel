0cfd14c08f17656f2ad54465f5c150ef4f224038
KXzeno <82054121+KXzeno@users.noreply.github.com>
2025-02-25 12:32:46 -0800

    chore: comply to linter
    - Remove unused type

--- app/components/forward/Landing/LandingHeader.tsx
+++ app/components/forward/Landing/LandingHeader.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 import useObserver from '@hooks/useObserver';
 import CQDispatcher from '@utils/core/CQDispatcher';
 import type {
-  EvtDispatcher,
+  // EvtDispatcher,
   Evt,
   ReducerState,
   ReducerAction
